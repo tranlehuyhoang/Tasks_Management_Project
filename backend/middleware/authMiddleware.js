91f6192fec0aa6fd154d6d2d6a44434a0d9873c0
Huyh Huyhu <2509roblox@gmail.com>
2023-10-30 20:36:59 +0700

    fix jwt 3

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,8 +5,9 @@ import User from '../models/userModel.js';
 
 const protect = asyncHandler(async (req, res, next) => {
     let token;
-    console.log(req.cookies)
     token = req.cookies.jwt1;
+    console.log(req.cookies)
+    console.log(req.cookies.jwt1)
 
     if (token) {
         try {
