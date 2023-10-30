83b5149a22a9e68bf03237f8ad590ce47e06da73
Huyh Huyhu <2509roblox@gmail.com>
2023-10-30 09:10:14 +0700

    fix vite api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       '/api': {
-        target: 'https://kanban-servers.onrender.com/        ',
+        target: 'https://kanban-servers.onrender.com/',
         changeOrigin: true,
       },
     },
