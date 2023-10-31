a5c9f6cbadc46f543df200fa0a3c86fc3b1353e6
Huyh Huyhu <2509roblox@gmail.com>
2023-10-31 07:45:21 +0700

    fix dns

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
 
-// https://vitejs.dev/config/
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
   plugins: [react()],
 
